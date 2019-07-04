6428c26a30e6aca0635df481a562f675695f2dbb
ycy <568082704@qq.com>
2019-07-04 18:33:50 +0800

    绑定鼠标事件

---
+++ main.js
@@ -0,0 +1,15 @@
+//按下去鼠标
+document.onmousedown=function(x) {
+  console.log(x)
+}
+
+//动鼠标
+// document.onmousemove=function(y) {
+//   console.log(y)
+// }
+
+
+//松开鼠标
+document.onmouseup=function(y) {
+  console.log(1)
+}
